99248d3ae9d720c955c813010cc752ec2ca07864
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-23 00:53:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array
+  for (var i = 0; i < musicians.length; i++) {
+    array[i] = "${musicians[i]} plays ${instruments[i]}"
+  }
+  return array
+}
