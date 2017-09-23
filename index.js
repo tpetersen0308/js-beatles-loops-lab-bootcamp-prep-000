da330dd0a83f62a110cabdcbf914c299d384cc72
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-23 01:28:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(musicians, instruments) {
-  var array
+  var array = []
   for (var i = 0; i < musicians.length; i++) {
     array.push("${musicians[i]} plays ${instruments[i]}")
   }
